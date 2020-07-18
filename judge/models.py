717b71a7f9b18b2e06bfddf37612cc7ca1ee673f
Marcin Jurczak <marcin12341@gmail.com>
2020-07-18 03:08:47 +0200

    Add models, views, urls and templates

--- judge/models.py
+++ judge/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    question_title = models.CharField(max_length=100)
+    question_description = models.CharField(max_length=1000)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_title
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Solution(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    solution_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.solution_text
--- judge/views.py
+++ judge/views.py
@@ -1,8 +1,44 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
+from .models import Question, Solution
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class IndexView(generic.ListView):
+    template_name = 'judge/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return all published questions."""
+        return Question.objects.order_by('-pub_date')[:]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'judge/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'judge/results.html'
+
+
+def send(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        sent_solution = question.solution_set.get(pk=request.POST['solution'])
+    except (KeyError, Solution.DoesNotExist):
+        return render(request, 'judge/detail.html', {
+            'question': question,
+            'error_message': "You didn't send a solution.",
+        })
+    else:
+        sent_solution.votes += 1
+        sent_solution.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('judge:results', args=(question.id,)))
+
