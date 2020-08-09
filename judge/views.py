ca2a324f7136ef9d50f7b73e3a1fd6968afb8888
marcin12341 <marcin.jurczak12@gmail.com>
2020-08-09 16:26:26 +0200

    Add django form

--- judge/views.py
+++ judge/views.py
@@ -1,10 +1,12 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponseForbidden
+from django.shortcuts import get_object_or_404, render
+from django.views.generic.edit import FormMixin
 from django.urls import reverse
 from django.views import generic
 
 from .models import Problem, Solution
 from .tasks import validate_solution
+from .forms import SendSolutionForm
 
 
 class ProblemIndexView(generic.ListView):
@@ -16,9 +18,28 @@ class ProblemIndexView(generic.ListView):
         return Problem.objects.order_by('-pub_date')[:]
 
 
-class ProblemDetailView(generic.DetailView):
+class ProblemDetailView(FormMixin, generic.DetailView):
     model = Problem
+    form_class = SendSolutionForm
     template_name = 'judge/detail.html'
+
+    def get_success_url(self):
+        return reverse('judge:send', kwargs={'pk': self.object.pk})
+
+    def get_context_data(self, **kwargs):
+        context = super(ProblemDetailView, self).get_context_data(**kwargs)
+        context['form'] = self.get_form()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        return super(ProblemDetailView, self).form_valid(form)
 
 
 class ResultsView(generic.DetailView):  # name?
