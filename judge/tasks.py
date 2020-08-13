fddaa5ce5870197744ff47a8205ee411d180278f
LiquidLemon <liquidlemon@protonmail.com>
2020-08-13 14:33:03 +0200

    Compilation proof of concept

--- judge/tasks.py
+++ judge/tasks.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+from subprocess import Popen, PIPE
+from tempfile import TemporaryDirectory
+
 from celery import shared_task
 
 from .models import Solution
@@ -8,13 +12,34 @@ def validate_solution(id: int):
     solution = Solution.objects.get(pk=id)
     solution.state = Solution.State.IN_PROGRESS
     solution.save()
-    problem = solution.problem
 
     try:
-        content = list(solution.get_sources().keys())[0]
-        solution.valid = int(len(content)) == len(problem.description)
+        solution.valid = validate(solution)
     except ValueError:
         solution.valid = False
 
     solution.state = Solution.State.VALIDATED
     solution.save()
+
+
+def validate(solution: Solution) -> bool:
+    tmp_dir = TemporaryDirectory()
+    source_path = Path(tmp_dir.name).joinpath("main.c")
+    with source_path.open("wb") as source_file:
+        source_file.write(solution.get_source().encode("utf-8"))
+
+    # call gcc
+    gcc = Popen(["gcc", "main.c"], cwd=tmp_dir.name)
+    gcc.wait()
+    # call resulting binary
+    program = Popen(["./a.out"], text=True, cwd=tmp_dir.name, stdout=PIPE)
+    (stdout, _) = program.communicate()
+    print(f"stdout: \n{stdout}")
+
+    tmp_dir.cleanup()
+
+    valid = stdout.strip() == solution.problem.description.strip()
+    if not valid:
+        print(f"expected: {solution.problem.description}")
+        print(f"actual: {stdout}")
+    return valid
