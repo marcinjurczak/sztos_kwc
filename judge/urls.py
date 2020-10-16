6aec117bfd580c6c8291f941138561da6549fad6
marcin12341 <marcin.jurczak12@gmail.com>
2020-10-16 19:16:20 +0200

    Add option to add Courses and Problems

--- judge/urls.py
+++ judge/urls.py
@@ -5,8 +5,10 @@ from . import views
 app_name = 'judge'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('courses', views.CourseListView.as_view(), name='courses'),
-    path('course/<int:pk>/problems', views.ProblemListView.as_view(), name='problems'),
-    path('problem/<int:pk>/', views.ProblemDetailView.as_view(), name='detail'),
+    path('courses/', views.CourseListView.as_view(), name='courses'),
+    path('courses/add/', views.CourseCreate.as_view(), name='add_course'),
+    path('course/<int:pk>/problems/', views.ProblemListView.as_view(), name='problems'),
+    path('course/<int:pk>/problem/add/', views.ProblemCreate.as_view(), name='add_problem'),
+    path('course/<int:course_pk>/problem/<int:problem_pk>/', views.ProblemDetailView.as_view(), name='detail'),
     path('<int:problem_id>/send/', views.send_solution, name='send'),
 ]
--- judge/views.py
+++ judge/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect, HttpResponseBadRequest, HttpResponse
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import get_object_or_404, redirect, render, get_list_or_404
 from django.urls import reverse, reverse_lazy
+from django.utils import timezone
 from django.views import generic
 from django.views.decorators.http import require_POST
 from django.views.generic import TemplateView
@@ -25,6 +27,23 @@ class CourseListView(generic.ListView):
         return Course.objects.filter(assigned_users__id=self.request.user.id)
 
 
+class CourseCreate(generic.CreateView):
+    template_name = 'judge/add_course.html'
+    model = Course
+    fields = ['name']
+    success_url = '/courses/'
+
+    def form_valid(self, form):
+        obj = form.save(commit=True)
+        obj.assigned_users.add(self.request.user)
+        obj.save()
+        return super(CourseCreate, self).form_valid(form)
+
+    def get_initial(self):
+        user = get_object_or_404(User, id=self.request.user.id)
+        return {'assigned_users': user}
+
+
 class ProblemListView(generic.ListView):
     template_name = 'judge/problems.html'
     context_object_name = 'latest_problem_list'
@@ -32,17 +51,44 @@ class ProblemListView(generic.ListView):
     def get_queryset(self):
         return Problem.objects.filter(course__id=self.kwargs.get('pk'))
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['pk'] = self.kwargs.get('pk')
+        context['course'] = Course.objects.get(id=self.kwargs.get('pk'))
+        return context
+
+
+class ProblemCreate(generic.CreateView):
+    template_name = 'judge/add_problem.html'
+    model = Problem
+    fields = ['course', 'title', 'description']
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.pub_date = timezone.now()
+        obj.save()
+        return super(ProblemCreate, self).form_valid(form)
+
+    def get_initial(self):
+        course = get_object_or_404(Course, id=self.kwargs.get('pk'))
+        return {'course': course}
+
+    def get_success_url(self):
+        course = get_object_or_404(Course, id=self.kwargs.get('pk'))
+        return reverse('judge:courses', args=course.id)
+
 
 class ProblemDetailView(FormMixin, generic.DetailView):
     model = Problem
     form_class = SendSolutionForm
     template_name = 'judge/detail.html'
+    pk_url_kwarg = 'problem_pk'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         solution = Solution.objects.filter(
             user__id=self.request.user.id,
-            problem__pk=self.kwargs.get('pk')
+            problem__pk=self.kwargs.get('problem_pk')
         ).last()
         context['user'] = self.request.user
         context["solution"] = solution
@@ -70,6 +116,3 @@ def send_solution(request, problem_id) -> HttpResponse:
     solution.save()
     validate_solution.delay(solution.id)
     return HttpResponseRedirect(reverse('judge:detail', args=(problem.id,)))
-
-
-
