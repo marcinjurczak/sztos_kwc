d4bd1ca65ed8e05b12879d3cd523b05e36ab52fe
marcin12341 <marcin.jurczak12@gmail.com>
2020-10-28 14:57:44 +0100

    Separate source code view

--- judge/urls.py
+++ judge/urls.py
@@ -10,8 +10,9 @@ urlpatterns = [
     path('course/<int:pk>/problems/', views.ProblemListView.as_view(), name='problems'),
     path('course/<int:pk>/problem/add/', views.ProblemCreate.as_view(), name='add_problem'),
     path('course/<int:course_pk>/problem/<int:problem_pk>/', views.ProblemDetailView.as_view(), name='detail'),
+    path('course/<int:course_pk>/problem/<int:problem_pk>/source/', views.SourceCodeView.as_view(), name='source_code'),
     path('course/<int:course_pk>/problem/<int:pk>/grades/', views.ProblemGradesView.as_view(), name='problem_grades'),
-    path('course/<int:course_pk>/problem/<int:problem_pk>/test/add', views.TestCaseCreate.as_view(), name='add_test_case'),
+    path('course/<int:course_pk>/problem/<int:problem_pk>/test/add/', views.TestCaseCreate.as_view(), name='add_test_case'),
     path('course/<int:course_pk>/problem/<int:problem_pk>/solution/<int:solution_pk>/download/', views.download_solution, name='download_solution'),
     path('<int:problem_id>/send/', views.send_solution, name='send'),
 ]
