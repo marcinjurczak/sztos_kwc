4320a62bb4c75dd28cb48b89c66282f9b99770e1
marcin12341 <marcin.jurczak12@gmail.com>
2020-11-03 16:25:21 +0100

    Add newly created users to 'students' group

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import Group
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -7,3 +9,14 @@ class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'auth/signup.html'
+
+    def post(self, request, *args, **kwargs):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+            user_group = Group.objects.get(name='students')
+            user.groups.add(user_group)
+            return redirect('login')
+        else:
+            return render(request, self.template_name, {'form': form})
