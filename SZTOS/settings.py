0b6f57d7bb73a61ea4873910630108bb8cdb92f3
marcin12341 <marcin.jurczak12@gmail.com>
2020-11-03 16:24:46 +0100

    Allow to populate database with .json file

--- SZTOS/settings.py
+++ SZTOS/settings.py
@@ -112,6 +112,12 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Fixtures
+
+FIXTURE_DIRS = (
+   '/accounts/fixtures/',
+)
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
